7e074942088a07e6460afa7a75db074622b87776
zohoMatt <mattzuo12@gmail.com>
2020-06-10 16:40:29 -0400

    feat: paint.js

---
+++ src/paint.js
@@ -0,0 +1,22 @@
+const paint = (
+    width,
+    height,
+    xPixels,
+    yPixels,
+    colorList
+) => {
+    const pixelWidth = width / xPixels;
+    const pixelHeight = height / yPixels;
+    let rects = '';
+    for (let x = 0; x < pixelWidth * xPixels; x += pixelWidth) {
+        for (let y = 0; y < pixelHeight * yPixels; y += pixelHeight) {
+            rects +=
+                `<rect width="${pixelWidth}" height="${pixelHeight}" x="${x}" y="${y}" fill="${colorList[x][y]}" />`;
+        }
+    }
+    return `
+        <svg width=${xPixels * pixelWidth} height=${yPixels * pixelHeight}>
+            ${rects}
+        </svg>    
+    `;
+};
